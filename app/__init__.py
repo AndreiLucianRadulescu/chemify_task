62bffa4c4d2b8d84da9293e2b62f7e1ac3f359b6
2577007r <2577007r@student.gla.ac.uk>
2023-11-19 01:15:55 +0000

    Created initial migration with alembic

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
 db = SQLAlchemy(app)
